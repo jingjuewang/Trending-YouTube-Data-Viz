849ffe0aae37f94b0a5f6c547e658c50da63dedd
Jinejue Wang <jwang193@dons.usfca.edu>
2018-05-07 14:18:17 -0700

    add grouped_bar plot and word clouds

---
+++ docs/figure.js
@@ -0,0 +1,299 @@
+var figure = {
+    "frames": [], 
+    "layout": {
+        "barmode": "group", 
+        "updatemenus": [
+            {
+                "bordercolor": "#FFFFFF", 
+                "yanchor": "bottom", 
+                "xanchor": "right", 
+                "y": 1.0, 
+                "active": 99, 
+                "x": -0.1
+            }, 
+            {
+                "bordercolor": "#FFFFFF", 
+                "yanchor": "bottom", 
+                "xanchor": "right", 
+                "y": 1.0, 
+                "buttons": [
+                    {
+                        "args": [
+                            {
+                                "visible": [
+                                    true, 
+                                    false, 
+                                    false, 
+                                    false
+                                ]
+                            }, 
+                            {
+                                "title": "Great Britain"
+                            }
+                        ], 
+                        "method": "update", 
+                        "label": "Great Britain"
+                    }, 
+                    {
+                        "args": [
+                            {
+                                "visible": [
+                                    false, 
+                                    true, 
+                                    false, 
+                                    false
+                                ]
+                            }, 
+                            {
+                                "title": "United States"
+                            }
+                        ], 
+                        "method": "update", 
+                        "label": "United States"
+                    }, 
+                    {
+                        "args": [
+                            {
+                                "visible": [
+                                    false, 
+                                    false, 
+                                    true, 
+                                    false
+                                ]
+                            }, 
+                            {
+                                "title": "Canada"
+                            }
+                        ], 
+                        "method": "update", 
+                        "label": "Canada"
+                    }, 
+                    {
+                        "args": [
+                            {
+                                "visible": [
+                                    false, 
+                                    false, 
+                                    false, 
+                                    true
+                                ]
+                            }, 
+                            {
+                                "title": "Germany"
+                            }
+                        ], 
+                        "method": "update", 
+                        "label": "Germany"
+                    }, 
+                    {
+                        "args": [
+                            {
+                                "visible": [
+                                    true, 
+                                    true, 
+                                    true, 
+                                    true
+                                ]
+                            }, 
+                            {
+                                "title": "Reset"
+                            }
+                        ], 
+                        "method": "update", 
+                        "label": "Reset"
+                    }
+                ], 
+                "bgcolor": "#d9d9d9", 
+                "active": -1, 
+                "x": -0.01
+            }
+        ], 
+        "title": "Average number of views in Categories"
+    }, 
+    "data": [
+        {
+            "name": "Great Britain", 
+            "ysrc": "jingjuewang:115:80aaa8", 
+            "xsrc": "jingjuewang:115:b57996", 
+            "marker": {
+                "color": "#fdb863"
+            }, 
+            "y": [
+                "Gaming", 
+                "News & Politics", 
+                "Education", 
+                "Entertainment", 
+                "Travel & Events", 
+                "Science & Technology", 
+                "Nonprofits & Activism", 
+                "People & Blogs", 
+                "Howto & Style", 
+                "Pets & Animals", 
+                "Music", 
+                "Comedy", 
+                "Sports", 
+                "Autos & Vehicles", 
+                "Film & Animation"
+            ], 
+            "x": [
+                750930.068443804, 
+                817484.689330544, 
+                578841.9481132076, 
+                3041638.533866345, 
+                1383177.0104166667, 
+                2983198.529411765, 
+                4922060.962962963, 
+                1910245.1063492063, 
+                582697.3692636073, 
+                841919.1550802139, 
+                10341287.033585409, 
+                2047067.312849162, 
+                1733597.8111040203, 
+                1477826.5514705882, 
+                2798806.4094182826
+            ], 
+            "type": "bar", 
+            "orientation": "h"
+        }, 
+        {
+            "name": "United States", 
+            "ysrc": "jingjuewang:115:1e08ae", 
+            "xsrc": "jingjuewang:115:d1c1cf", 
+            "marker": {
+                "color": "#e66101"
+            }, 
+            "y": [
+                "Gaming", 
+                "News & Politics", 
+                "Education", 
+                "Entertainment", 
+                "Travel & Events", 
+                "Science & Technology", 
+                "Nonprofits & Activism", 
+                "People & Blogs", 
+                "Howto & Style", 
+                "Pets & Animals", 
+                "Music", 
+                "Comedy", 
+                "Sports", 
+                "Autos & Vehicles", 
+                "Film & Animation", 
+                "Shows"
+            ], 
+            "x": [
+                1269492.5573770492, 
+                367866.21614967997, 
+                527812.7954186414, 
+                1512638.160488443, 
+                884091.2585669782, 
+                990346.7014508928, 
+                3462796.5208333335, 
+                1131034.8311965812, 
+                693378.5505310037, 
+                602219.7281831187, 
+                3115687.003106332, 
+                1262042.1443548386, 
+                1629750.4465699208, 
+                1394790.738292011, 
+                2099629.80474111, 
+                199312.05555555556
+            ], 
+            "type": "bar", 
+            "orientation": "h"
+        }, 
+        {
+            "name": "Canada", 
+            "ysrc": "jingjuewang:115:1e08ae", 
+            "xsrc": "jingjuewang:115:53b40e", 
+            "marker": {
+                "color": "#b2abd2"
+            }, 
+            "y": [
+                "Gaming", 
+                "News & Politics", 
+                "Education", 
+                "Entertainment", 
+                "Travel & Events", 
+                "Science & Technology", 
+                "Nonprofits & Activism", 
+                "People & Blogs", 
+                "Howto & Style", 
+                "Pets & Animals", 
+                "Music", 
+                "Comedy", 
+                "Sports", 
+                "Autos & Vehicles", 
+                "Film & Animation", 
+                "Shows"
+            ], 
+            "x": [
+                797170.5715715715, 
+                355804.25873632194, 
+                464306.9837837838, 
+                1010689.1613807983, 
+                360720.2544169611, 
+                1119171.761966365, 
+                1708662.5909090908, 
+                722012.5634122607, 
+                745042.9434782609, 
+                647205.1471698113, 
+                3333008.098031648, 
+                996006.7051749271, 
+                1094258.7259663034, 
+                598059.4810606061, 
+                1388653.4428093645, 
+                610686.4324324324
+            ], 
+            "type": "bar", 
+            "orientation": "h"
+        }, 
+        {
+            "name": "Germany", 
+            "ysrc": "jingjuewang:115:e1ffed", 
+            "xsrc": "jingjuewang:115:a8d324", 
+            "marker": {
+                "color": "#5e3c99"
+            }, 
+            "y": [
+                "Gaming", 
+                "News & Politics", 
+                "Education", 
+                "Entertainment", 
+                "Travel & Events", 
+                "Science & Technology", 
+                "Nonprofits & Activism", 
+                "People & Blogs", 
+                "Howto & Style", 
+                "Trailers", 
+                "Pets & Animals", 
+                "Music", 
+                "Comedy", 
+                "Sports", 
+                "Autos & Vehicles", 
+                "Film & Animation", 
+                "Shows"
+            ], 
+            "x": [
+                319401.95487510075, 
+                244379.41042345276, 
+                143607.63209076176, 
+                501118.17545849876, 
+                125116.98214285714, 
+                544313.6666666666, 
+                701728.2602040817, 
+                349002.7471024571, 
+                300611.6937151949, 
+                8804.0, 
+                402357.77348066296, 
+                2118382.628938907, 
+                640936.5057012542, 
+                709129.9601699415, 
+                217746.82643312102, 
+                699723.6905725853, 
+                463496.8552631579
+            ], 
+            "type": "bar", 
+            "orientation": "h"
+        }
+    ]
+}
